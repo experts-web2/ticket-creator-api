258e705b4c3821fbed9fc42edcf228c8a9be2a7d
Experts_web2 <experts.web2@gmail.com>
2022-07-26 20:53:12 +0500

    CorsIssue

--- app.js
+++ app.js
@@ -10,22 +10,15 @@ const morgan = require("morgan");
 app.use(morgan("dev"));
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://card-management.s3-website-us-west-2.amazonaws.com/",
+      "http://localhost:3000",
+    ],
+  })
+);
 app.use("/api/ticket", ticketRoute);
-https: app.use(function (req, res, next) {
-  var allowedOrigins = ["http://localhost:3000/"];
-  var origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) > -1) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-  return next();
-});
 
 const portToListen = 3030;
 app.listen(port, () => {
